f4e46c073353f81850eb374f42f9677e86766b5f
prinssharma <21mcmc33@uohyd.ac.in>
2023-08-03 11:49:30 +0530

    ASCII value

---
+++ PrintASCII.py
@@ -0,0 +1,21 @@
+# capital letters
+print("--------------")
+for i in range(65,91):
+    print(chr(i),"->",i)
+
+print("--------------")
+# small letters
+for i in range(97,123):
+    print(chr(i),"->",i)
+print("--------------")
+
+# special character
+for i in range(91,97):
+    print(chr(i),"->",i)
+print("--------------")
+
+# special letters
+for i in range(123,127):
+    print(chr(i),"->",i)
+print("--------------")
+
